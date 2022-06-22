caa01dfca4dc0cbce78ef9d66281218533b83adf
Nikita <rankuru49@gmail.com>
2022-06-22 15:30:51 +0300

    Ready

--- website/main/models.py
+++ website/main/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField("Название", max_length=50)
+    task = models.TextField("Описание")
+    created_on = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    completed = models.BooleanField('Выполнена?')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Задача"
+        verbose_name_plural = "Задачи"
--- website/main/views.py
+++ website/main/views.py
@@ -1,12 +1,27 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    task = Task.objects.order_by('completed','created_on')
+    return render(request, 'index.html', {'title':'Главная страница', 'tasks':task})
 
 def about(request):
     return render(request, 'about.html')
 
-def home(request):
-    return render(request, 'home.html')
+def create(request):
+    error = ''
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = "Форма не заполнена"
+    form = TaskForm()
+    context = {
+        'form':form,
+        'error':error
+    }
+    return render(request, 'create.html', context)
